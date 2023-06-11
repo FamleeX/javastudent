1fe0e3a824b2e18dc5ba8ce5350edba158a38eae
Carter Lee <cartermario@protonmail.com>
2023-06-11 16:36:36 -0700

    finalized assignment 3

--- Assignment3/NumbersRangeSum.java
+++ Assignment3/NumbersRangeSum.java
@@ -1,23 +1,25 @@
-package Assignment3;
-
 import static java.lang.System.out;
 public class NumbersRangeSum {
 	
 	public static void main (String[] args) {
 		
 		//These arguments parse and store the command line arguments
-		int number1 = 6;//Integer.parseInt(args[0]);
-		int number2 = 10;//Integer.parseInt(args[1]);
+		int number1 = Integer.parseInt(args[0]);
+		int number2 = Integer.parseInt(args[1]);
 		
 		//This variable stores the result of the sum of a range of numbers
+		//and calls a recursive method to get the sum of the range
 		int sum = getSum(number1, number2);
 		
-		//The line to print
-		String string = "The sum of natural numbers in range [" + 
-						   number1 + ", " + number2 + "] is " + sum;
-		out.println(string);
-		//System.out.println("The sum of natural numbers in range [" + 
-		//				   number1 + ", " + number2 + "] is " + sum);
+		//Create the message to print
+		String message = "The sum of natural numbers in range";
+		
+		//Create the format string. I could include message in the format,
+		//but research for C told me it is bad practice. Better safe than sorry.
+		String format = "%s [%d, %d] %s %d %n";
+		
+		//Print the original range and resulting sum
+		out.printf(format, message, number1, number2, "is", sum);
 		
 	}
 	
@@ -25,6 +27,7 @@ public class NumbersRangeSum {
 	public static int getSum(int startNumber, int endNumber) {
 		
 		//This if statement would swap the arguments if startNumber is the bigger number
+		//makes the recursive function below shorter when done this way.
 		if (startNumber > endNumber) {
 			int temp = endNumber;
 			endNumber = startNumber;
@@ -34,10 +37,14 @@ public class NumbersRangeSum {
 		//This if statement will end the recursion and return the last number
 		//if it is the last number remaining
 		if (startNumber == endNumber) return endNumber;
-		//sumOf variable stores the sum
-		int sumOf = startNumber + getSum(startNumber + 1, endNumber);
+		
+		//sum variable stores the sum. To add the next number in the range
+		//recursion is used by calling the startNumber, then calling method with
+		//startNumber + 1 added to close in the range
+		int sum = startNumber + getSum(startNumber + 1, endNumber);
+		
 		//once recursion is done adding all numbers this returns the result as an int
-		return sumOf;
+		return sum;
 		
 	}
 	
--- Assignment3/TriangularTest.java
+++ Assignment3/TriangularTest.java
@@ -1,31 +1,41 @@
-package Assignment3;
-
+import static java.lang.System.out;
 public class TriangularTest {
 	
+	//Main method of class
+	//Exit code 1 is not enough arguments
 	public static void main (String[] args) {
 		
-		int number1 = 5;
-		int number2 = 3;
-		int number3 = 6;
+		//Checks if there are three command line arguments and exits program if there isn't
+		if (args.length != 3) {
+			out.println("Please enter three integers from the command line as arguments");
+			System.exit(1);
+		}
 		
-		//These variables store a parsed int value from command line arguments
-		//int number1 = Integer.parseInt(args[0]);
-		//int number2 = Integer.parseInt(args[1]);
-		//int number3 = Integer.parseInt(args[2]);
+		//Assign command line int arguments to local variables
+		int number1 = Integer.parseInt(args[0]);
+		int number2 = Integer.parseInt(args[1]);
+		int number3 = Integer.parseInt(args[2]);
 		
-		if (isTriangular(number1, number2, number3))
-			System.out.println("True");
-		else 
-			System.out.println("False");
+		//See if the three integers are sides of a triangle and store the value
+		boolean isTriangular = isTriangular(number1, number2, number3);
+		
+		//If they were sides of a triangle print True, otherwise print False
+		if (isTriangular) out.println("True");
+		else out.println("False");
 		
 	}
 	
-	public static boolean isTriangular(int side1, int side2, int side3) {
-		
-		if (side1 >= side2 + side3) return false;
-		else if (side2 >= side1 + side3) return false;
-		else if (side3 >= side1 + side2) return false;
-		else return true;
+	//This method checks if three integers are sides of a triangle
+	//arguments are a, b, c to represent sides of a triangle
+	//order of numbers does not matter
+	public static boolean isTriangular(int a, int b, int c) {
+		//evaluates if any side is greater than the sum of the other two
+		//if so the numbers are not from a triangle and returns false
+		if (a >= b + c || b >= a + c || c >= a + b) 
+			return false;
+		//Otherwise the sides are from a triangle
+		else 
+			return true;
 		
 	}
 	
