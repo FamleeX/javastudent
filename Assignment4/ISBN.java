f776ac9a026858574102c8d99fc095ebac62525d
Carter Lee <cartermario@protonmail.com>
2023-06-24 18:04:54 -0700

    finalized assignment 4

--- Assignment4/ISBN.java
+++ Assignment4/ISBN.java
@@ -1,44 +1,76 @@
-import java.util.*;
-import java.io.*;
+/*
+ * Created by Carter Lee
+ * ISBN - Calculates checksum digit from a command line
+ * ISBN number argument
+ */
 import static java.lang.System.out;
 public class ISBN {
 	
+	//'immutable' String constant to display a message for not enough characters 
+	private static final String NOT_ENOUGH_CHARACTERS = "Please provide a 9 digit String representing an ISBN number";
+	
 	public static void main(String[] args) {
 		
-		//This if statement checks if the command line argument is 9 characters
-		if (args[0].length() != 9) {
-			out.println("The ISBN number must be 9 digits long");
+		//command line argument for first 9 digits of ISBN number
+		String ISBN = args[0];//"032149805";
+		//stores the calculated checksum digit
+		String checksum = calculateChecksum(ISBN);
+		//appends the checksum to the ISBN number
+		ISBN += checksum;
+		//prints the result
+		out.println("The ISBN number would be " + ISBN);
+		
+	}
+	
+	//This method can be used to pass a String of 9 numbers (command line argument)
+	//to return a String of the checksum digit of the ISBN number
+	public static String calculateChecksum(String ISBN) {
+		
+		//Checks if provided ISBN string is 9 digits
+		if (ISBN.length() != 9) {
+			out.println(NOT_ENOUGH_CHARACTERS);
 			System.exit(1);
 		}
-
-		//store the argument as a local variable
-		//ISBN is the 9 digit book number
-		final String ISBN = args[0];
 		
-		//store the result
-		String checksum = calculateChecksum(ISBN);
-		//print the result
-		out.println("The ISBN number would be " + ISBN + checksum);
+		//split the string to parse each character to an int value
+		String[] characters = ISBN.split("");
 		
-	}
-
-	public static String calculateChecksum(String input) {
-		
+		//sum is used to store the sum of (d1 + 2d2 + 3d3 + ... + 10d10)
+		//which is part of the calculation for the checksum digit
 		int sum = 0;
-		for (int i = 0; i < input.length(); i++) {
-			int digit = Character.getNumericValue(input.charAt(i));
-			sum += (i + 1) * digit;
+		for (int i = 0, j = 1; i < characters.length; i++, j++) {
+			int tempInt = Integer.parseInt(characters[i]);
+			sum += (j * tempInt);
 		}
-		//checksumDigit stores the checksum as an int
-		int checksumDigit = sum % 11;
-		//checksum will hold the checksum as a String
-		String checksum = "";
-		//If the checksum is 10, it will set result to X per ISBN conventions
-		if (checksumDigit == 10) checksum += "X";
-		//Otherwise result will be set to the digit
-		else checksum += checksumDigit;
-		//return the checksum
-		return checksum;
-
+		
+		//remainder is used to exit loop if the checksum digit was calculated
+		int remainder = 1;
+		//checksum is used to track which digit satisfies the checksum calculation
+		//value is set to -1 to increase with the while loop while starting at 0
+		int checksum = -1;
+		//the loop will exit if the remainder is 0 as the checksum was calculated
+		
+		while (remainder != 0) {
+			
+			//increase checksum
+			checksum++;
+			//calculate remainder to determine if checksum is valid
+			remainder = (sum + (checksum * 10)) % 11;
+			
+		}
+		
+		//returns the calculated checksum
+		if (checksum < 10) return Integer.toString(checksum);
+		else return "X";
+		
 	}
+	
+	//This method is just like above, but can be used to pass a int value as a String 
+	//for the ISBN number to the above method. Unused in my tests
+	public static String calculateChecksum(int ISBN) {
+		
+		return calculateChecksum(Integer.toString(ISBN));
+		
+	}
+	
 }
--- Assignment4/Point.java
+++ Assignment4/Point.java
@@ -1,14 +1,17 @@
-// Online Java Compiler
-// Use this online editor to compile and run Java code online
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+/*
+ * Created by Carter Lee
+ * Point - Create a Point data type and 
+ * test functionality based on doubles
+ * command line arguments
+ */
 import static java.lang.System.out;
 public class Point {
 	
-	public double x = 0;
-	public double y = 0;
-	public double z = 0;
+	private static final String NOT_ENOUGH_ARGS = "Please enter 6 command line arguments";
+
+	private double x = 0;
+	private double y = 0;
+	private double z = 0;
 	
 	public Point(double x, double y, double z) {
 		this.x = x;
@@ -16,6 +19,8 @@ public class Point {
 		this.z = z;
 	}
 	
+	//This method calculates the Euclidean distance between the
+	//current instance Point and another specified Point as an argument
 	public double distanceTo(Point q) {
 		double x = (this.x-q.x) * (this.x-q.x);
 		double y = (this.y-q.y) * (this.y-q.y);
@@ -24,27 +29,40 @@ public class Point {
 		double distance = Math.sqrt(x + y + z);
 		
 		distance = Math.round(distance * 100);
-		distance = distance/100;
+		distance /= 100;
 		
 		return distance;
 	}
 	
+	//This method returns the point as a String value of "(x,y,z)"
 	public String toString() {
 		String string = "(" + this.x + "," + this.y + "," + this.z + ")";
 		return string;
 	}
 	
-	
 	public static void main(String[] args) {
-		//if (args.length != 6) {
-			//out.println("Sorry the correct args weren't detected");
-		//}
+
+		//Checks for correct number of arguments
+		if (args.length != 6) {
+			out.println(NOT_ENOUGH_ARGS);
+			System.exit(1);
+		}
+
+		//Parse command line arguments into doubles
+		double[] dargs = new double[args.length];
+		for (int i = 0; i < dargs.length; i++) {
+			dargs[i] = Double.parseDouble(args[i]);
+		}	
+
+		//create two Point objects
+		Point point1 = new Point(dargs[0],dargs[1],dargs[2]);//Point(2.1, 3.0, 3.5);
+		Point point2 = new Point(dargs[3],dargs[4],dargs[5]);//Point(4.0, 5.2, 3.5);
 		
-		Point point1 = new Point(2.1, 3.0, 3.5);
-		Point point2 = new Point(4, 5.2, 3.5);
-		
+		//convert the Point objects to a string to print
 		out.println("The first point is " + point1.toString());
 		out.println("The second point is " + point2.toString());
+		
+		//calculate the Euclidean distance
 		out.println("Their Euclidean distance is " + point1.distanceTo(point2));
 	}
 	
